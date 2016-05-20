9636f891a2cc2a82b75bfe8194d4a9bda78c94f6
kawayikp <7783387@qq.com>
2016-05-19 21:50:04 -0700

    changed by Yue Liu

---
+++ src/coen275project/Food.java
@@ -0,0 +1,5 @@
+package coen275project;
+
+public class Food {
+
+}
---
+++ src/coen275project/TestDriver.java
@@ -0,0 +1,8 @@
+package coen275project;
+
+public class TestDriver {
+	public static void main(String[] args) {
+		System.out.println("test");
+	}
+
+}
