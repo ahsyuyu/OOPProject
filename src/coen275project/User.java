60803604ba908c2b98354a9fa4e41c7d88ac26d0
ahsyuyu <ahsyuyu@gmail.com>
2016-05-25 15:42:06 -0700

    add implements Serializable and test package

--- src/coen275project/User.java
+++ src/coen275project/User.java
@@ -1,6 +1,8 @@
 package coen275project;
 
-public class User {
+import java.io.Serializable;
+
+public class User implements Serializable {
     private String name;
     private int id;
     private Card card;
